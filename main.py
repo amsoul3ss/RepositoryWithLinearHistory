4919937293f7159b1f29f387ebee5a6b7ab39d41
Kirill Aseev <amsoul3ss.main@gmail.com>
2024-04-13 19:41:54 +0400

    Added function fib

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
 print("Hello, World!")
+
+n = int(input("Input n: "))
+print(f"Result fib({n}) = {fib(n)}")
